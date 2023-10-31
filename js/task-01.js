7278e5f27a61bfc97c2cc88df9b8c7c97c529c4f
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-01 00:20:55 +0200

    edit

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,13 @@
-const categories = document.querySelector('ul');
-const totall = categories.map(category => console.log(category))
-console.log(categories);
+const categories = document.querySelector("#categories");
+
+const categoriesItemsNum = categories.children.length;
+console.log("Number of categories:", categoriesItemsNum);
+
+
+
+const categoryName = categories.getElementsByTagName("h2");
+
+
+for (let i of categoryName) {
+  console.log("Category:", i.innerText);
+}
