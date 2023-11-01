30bb90839526ea257f0c15aa13f5262ed51ec4a5
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-01 23:10:22 +0200

    seccond task done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,36 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+// цей варінат додає елементи послідовно.
+const addIngredients = (arr) => {
+  arr.map((meal) => {
+    let item = document.createElement("li");
+    item.textContent = meal;
+    item.classList.add("item");
+    ingredientsList.append(item);
+  });
+};
+
+
+//  другий варіант додає все одним махом а не поступово в тілі циклу
+const addIngredients1 = (arr) => {
+  const items = arr.map((meal) => {
+    const item = document.createElement("li");
+    item.textContent = meal;
+    item.classList.add("item");
+    console.log(item);
+    return item;
+  });
+  ingredientsList.append(...items);
+};
+
+addIngredients(ingredients);
+addIngredients1(ingredients);
