504917d5b5606ad7a7e1234395e3da0154e2a8e1
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-05 23:22:41 +0200

    edit

--- js/task-04.js
+++ js/task-04.js
@@ -5,13 +5,12 @@ const value = document.querySelector("#value");
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 let counterValue = 0;
 
-// додав заборону від"ємних значень умовою if
 function decrementValue() {
-  if (counterValue > 0) {
+  
     counterValue -= 1;
     value.textContent = counterValue;
   }
-}
+
 function incrementValue() {
   counterValue += 1;
   value.textContent = counterValue;
