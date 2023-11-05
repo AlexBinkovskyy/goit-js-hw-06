0ad7d83a68216355de1b9d0cd5e77c305fd6eafa
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-05 19:43:05 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.email.value;
+  const pass = form.password.value;
+  if (!email || !pass) {
+    window.alert("Не заповнені усі необхідні поля");
+  } else {
+    const result = { [form.email.name]: email, [form.password.name]: pass };
+    console.log(result);
+    form.reset();
+  }
+}
