2cbf0ecc975956c2c5b96a6832991054e92cd8e4
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-06 12:02:43 +0200

    edit

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,13 @@
 const body = document.querySelector('body');
 const changeColorBtn = document.querySelector('.change-color')
 changeColorBtn.addEventListener("click", getRandomHexColor)
+const resultBox = document.querySelector('.color')
+
 
 function getRandomHexColor() {
-  body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+  body.style.backgroundColor = resultBox.textContent = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+    
  }
 
