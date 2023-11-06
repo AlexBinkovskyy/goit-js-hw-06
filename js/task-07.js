977bc6b45fa462b4b04408636a954c935c5178f2
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-06 20:35:34 +0200

    edit

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('#font-size-control');
 const sample = document.querySelector('#text');
-input.addEventListener('change', changeFont)
+input.addEventListener('input', changeFont)
+sample.style.fontSize = `${input.value}px`;
 
 function changeFont (event)  {
    sample.style.fontSize = `${event.currentTarget.value}px`
